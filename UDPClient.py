a28619f3b00b66ca0fe1520090ad77d9b10e32fb
elien2016 <eislander16@gmail.com>
2023-02-25 22:38:21 -0700

    finish testing on IBM Cloud Kubernetes Service

--- UDPClient.py
+++ UDPClient.py
@@ -1,13 +1,15 @@
+# I deleted my deployment on IBM Cloud so the IP address below will NOT work.
+# But I included this file as an example.
 import requests
 
-fs_ip = 'localhost'
-fs_port = 9090
+fs_ip = '159.122.174.86'
+fs_port = 30002
 
 data = {
     'hostname': 'fibonacci.com',
-    'ip': '172.18.0.1',
-    'as_ip': '172.18.0.1',
-    'as_port': 53533
+    'ip': '159.122.174.86',
+    'as_ip': '159.122.174.86',
+    'as_port': 30001
 }
 
 response = requests.put(f'http://{fs_ip}:{fs_port}/register', json=data)
